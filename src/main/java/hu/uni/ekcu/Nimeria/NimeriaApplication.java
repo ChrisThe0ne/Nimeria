efb44303ce6d91c338d0bb0313026b6d3f8e2d8a
Bencsik Krisztián <61544402+ChrisThe0ne@users.noreply.github.com>
2022-03-27 14:46:00 +0200

    Initial commit

---
+++ src/main/java/hu/uni/ekcu/Nimeria/NimeriaApplication.java
@@ -0,0 +1,13 @@
+package hu.uni.ekcu.Nimeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/uni/ekcu/Nimeria/NimeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.uni.ekcu.Nimeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
